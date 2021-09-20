a33671bbefcab767dc1b300ece8fe3009744afb7
mohon63 <mohonhossen63@gmail.com>
2021-09-20 21:32:15 +0600

    react core concepts recap

--- src/App.js
+++ src/App.js
@@ -4,22 +4,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyComponent brand="MicroSoft" price="10000"></MyComponent>
+      <MyComponent brand="Corola" price="122000"></MyComponent>
+      <MyComponent brand="Sony" price="20000"></MyComponent>
+      <MyComponent brand="Nokia" price="15000"></MyComponent>
     </div>
   );
 }
 
+function MyComponent(props) {
+  console.log(props);
+  const myStyle = {
+    backgroundColor: '#574180',
+    color: 'white',
+    border: '3px solid yellow',
+    borderRadius: '10px',
+    margin: '15px',
+    padding: '5px'
+  }
+  return (
+    <div style={myStyle}>
+      <h1>Yo Yo mama! {props.brand}</h1>
+      <p>Asking money: {props.price}</p>
+      <p style={{ color: 'magenta', fontWeight: 'bold' }}>I can write my own component</p>
+    </div>
+  )
+}
 export default App;
